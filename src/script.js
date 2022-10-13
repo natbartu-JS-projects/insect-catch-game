9ccf683d009763708aaf148e277cd619bd2811e0
natacha <nat_bartu@hotmail.com>
2022-10-13 13:33:07 +1000

    bring all the elements into the DOM

--- src/script.js
+++ src/script.js
@@ -1,1 +1,7 @@
 const screens = document.querySelectorAll(".screen");
+const chooseInsectBtns = document.querySelectorAll(".choose-insect-btn");
+const startBtn = document.getElementById("start-btn");
+const timeElement = document.getElementById("time");
+const scoreElement = document.getElementById("score");
+const message = document.getElementById("message");
+const gameContainer = document.getElementById("game-container");
