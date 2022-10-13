8b644ded2da2f5d4bd461ce3aa2c27ea8c38b287
natacha <nat_bartu@hotmail.com>
2022-10-13 13:38:15 +1000

    add event listener for the game button, when clicked it will move up

--- src/script.js
+++ src/script.js
@@ -5,3 +5,9 @@ const timeElement = document.getElementById("time");
 const scoreElement = document.getElementById("score");
 const message = document.getElementById("message");
 const gameContainer = document.getElementById("game-container");
+
+let seconds = 0;
+let score = 0;
+let selectedInsect = {};
+
+startBtn.addEventListener("click", () => screens[0].classList.add("up"));
